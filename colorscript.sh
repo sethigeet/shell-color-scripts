46436b3aea9b97fa22066a99113a1352ba9680d2
Derek Taylor <contact@unabot.com>
2020-03-28 16:54:55 -0500

    Minor edit.

--- colorscript.sh
+++ colorscript.sh
@@ -2,7 +2,7 @@
 
 # Simple CLI for shell-color-scripts
 
-DIR_COLORSCRIPTS="/opt/shell-color-scripts"
+DIR_COLORSCRIPTS="/opt/shell-color-scripts/colorscripts"
 fmt_help="  %-20s\t%-54s\n"
 list_colorscripts="$(/usr/bin/ls "${DIR_COLORSCRIPTS}" | cut -d ' ' -f 1 | nl)"
 length_colorscripts="$(/usr/bin/ls "${DIR_COLORSCRIPTS}" | wc -l)"
